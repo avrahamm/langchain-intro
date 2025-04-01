fb1bbe8caea03e9cf0df46ad1c2f2bb0217fee0a
avrahamm <papaabramg@gmail.com>
2025-04-01 15:16:18 +0300

    stage 2 solution

--- main.py
+++ main.py
@@ -1,18 +1,54 @@
 # install the required packages:
-# pip install langchain-core python-dotenv langchain-openai
+# pip install langchain-groq langchain-core python-dotenv
 
 from langchain_core.prompts import PromptTemplate
-from langchain_openai import ChatOpenAI
+from langchain_core.prompts import FewShotPromptTemplate
+from langchain_groq import ChatGroq
 import dotenv
-import os
 
 dotenv.load_dotenv()
 
-llm = ChatOpenAI(api_key=os.getenv("OPENAI_API_KEY")) # if using a different base_url and model, pass them as well
 
-question = input()
-template = PromptTemplate.from_template("You are a helpful assistant who answers questions users may have. You are asked: {question}.")
+def main():
+    """
+    To run tests
+    :return:
+    """
 
-prompt = template.invoke({"question": question})
-response = llm.invoke(prompt)
-print(response.content)
+    llm = ChatGroq(
+        model="llama-3.3-70b-versatile",
+        temperature=0.5,
+        max_retries=2
+    )
+
+    examples = [
+        {"input": "Jupiter", "output": """
+        Jupiter is the largest planet in the solar system.
+        It is a gas giant primarily composed of hydrogen and helium.
+        It has a Great Red Spot, a massive storm, and at least 79 known moons, including Ganymede, the largest moon in the solar system.
+        """
+         },
+        {"input": "Mars", "output": """
+        Mars is the fourth planet from the Sun.
+         It has a thin atmosphere composed mainly of carbon dioxide and is known for its red appearance due to iron oxide on its surface.
+    Notable features include Olympus Mons, the largest volcano in the solar system, and Valles Marineris, a vast canyon system.
+        """},
+    ]
+
+    example_template = PromptTemplate.from_template("Q: {input}\nA: {output}")
+
+    few_shot_prompt = FewShotPromptTemplate(
+        examples=examples,
+        example_prompt=example_template,
+        suffix="{question}",
+        input_variables=["question"],
+    )
+    planet = input()
+    final_prompt = few_shot_prompt.format(question=f"Tell me about {planet} planet?")
+
+    response = llm.invoke(final_prompt)
+
+    print(response.content)
+
+if __name__ == "__main__":
+    main()
