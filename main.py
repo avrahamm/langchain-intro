aeffd0f549ab18db57f5e72ad507fc6b722347d0
avrahamm <papaabramg@gmail.com>
2025-04-01 20:56:47 +0300

    stage 3 solution

--- main.py
+++ main.py
@@ -1,54 +1,35 @@
-# install the required packages:
-# pip install langchain-groq langchain-core python-dotenv
-
-from langchain_core.prompts import PromptTemplate
-from langchain_core.prompts import FewShotPromptTemplate
-from langchain_groq import ChatGroq
+# Install the required packages.
+# pip install langchain-chroma
+from langchain_community.document_loaders import DirectoryLoader, TextLoader
+from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
+from langchain_chroma import Chroma
+import os
 import dotenv
 
 dotenv.load_dotenv()
 
 
 def main():
-    """
-    To run tests
-    :return:
-    """
+    # Load all text files from the 'planets/' directory
+    loader = DirectoryLoader("planets", glob="*.txt", loader_cls=TextLoader)
+    documents = loader.load()
 
-    llm = ChatGroq(
-        model="llama-3.3-70b-versatile",
-        temperature=0.5,
-        max_retries=2
+    # Initialize HuggingFace embeddings model.
+    embeddings_model = HuggingFaceInferenceAPIEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2",
+        api_key=os.getenv("HUGGINGFACE_API_KEY")
     )
 
-    examples = [
-        {"input": "Jupiter", "output": """
-        Jupiter is the largest planet in the solar system.
-        It is a gas giant primarily composed of hydrogen and helium.
-        It has a Great Red Spot, a massive storm, and at least 79 known moons, including Ganymede, the largest moon in the solar system.
-        """
-         },
-        {"input": "Mars", "output": """
-        Mars is the fourth planet from the Sun.
-         It has a thin atmosphere composed mainly of carbon dioxide and is known for its red appearance due to iron oxide on its surface.
-    Notable features include Olympus Mons, the largest volcano in the solar system, and Valles Marineris, a vast canyon system.
-        """},
-    ]
+    # Create a Chroma vector store from the loaded documents and HuggingFace embeddings.
+    db = Chroma.from_documents(documents, embeddings_model)
 
-    example_template = PromptTemplate.from_template("Q: {input}\nA: {output}")
+    # Perform a similarity search on the vector store with a sample query.
+    query = input()  #"What makes Saturn unique?"
+    docs = db.similarity_search(query)
 
-    few_shot_prompt = FewShotPromptTemplate(
-        examples=examples,
-        example_prompt=example_template,
-        suffix="{question}",
-        input_variables=["question"],
-    )
-    planet = input()
-    final_prompt = few_shot_prompt.format(question=f"Tell me about {planet} planet?")
+    # Print the content of the most similar document.
+    print(docs[0].page_content)
 
-    response = llm.invoke(final_prompt)
-
-    print(response.content)
 
 if __name__ == "__main__":
     main()
